c759c401f4e2b35bcc79705beebb2c19afdad303
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-17 17:19:08 -0600

    FIx the redirect in POST

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -4,33 +4,33 @@ const Profile = require('../models/Profile');
 
 // Get all Profiles //
 router.get('/', (req, res) => {
-    Profile.find({}).then(profiles => {
-        res.json(profiles);
-    });
+	Profile.find({}).then(profiles => {
+		res.json(profiles);
+	});
 });
 
 // Get profile by id
 router.get('/:id', (req, res) => {
-    Profile.findById(req.params.id).then(profile => res.json(profile));
+	Profile.findById(req.params.id).then(profile => res.json(profile));
 });
 
 // POST new Profile //
 router.post('/', (req, res) => {
-    const newProfile = req.body;
-    Profile.create(newProfile).then(profile => res.json('/'));
+	const newProfile = req.body;
+	Profile.create(newProfile).then(profile => res.json(profile));
 });
 
 // PUT edit Profile //
 router.put('/:id/edit', (req, res) => {
-    Profile.findOneAndUpdate({ _id: req.params.id }, req.body, {
-        new: true
-    }).then(profile => res.json(profile));
+	Profile.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true
+	}).then(profile => res.json(profile));
 });
 
 // DELETE Profile //
 router.delete('/:id', (req, res) => {
-    Profile.findOneAndDelete({ _id: req.params.id }).then(profile =>
-        res.json(profile)
-    );
+	Profile.findOneAndDelete({ _id: req.params.id }).then(profile =>
+		res.json(profile)
+	);
 });
 module.exports = router;
