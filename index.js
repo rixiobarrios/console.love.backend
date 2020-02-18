a60b7b160ff144f7711871fc2dd6b9e4de69baef
jnlsings <jnlsings@gmail.com>
2020-02-18 12:45:17 -0600

    Move cors up above express and .set in index

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const methodOverride = require('method-override');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.set('port', process.env.PORT || 4000);
 
 app.use(express.json());
 
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/profiles', profilesController);
